a67a5e7a1e93c6bb61a06b37fd97e33648a1513a
Enxhi Salaj <enxhi_salaj@yahoo.com>
2021-01-17 18:20:44 -0500

    fixed server

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
+// Dependencies
 const express = require("express");
 const mongoose = require("mongoose");
 const logger = require("morgan");
 
+// PORT
 const PORT = process.env.PORT || 3000;
 
 const app = express();
@@ -10,14 +12,17 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/workout", {
+// Connection with the database
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
+
 // routes
 app.use(require("./routes/api-routes.js"));
 app.use(require("./routes/html-routes.js"));
 
+// app event listener
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
   });
